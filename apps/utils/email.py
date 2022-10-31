58fcd230d33300b491266cfaf2e6322ce685786a
Joel Borrero <joeborrero13@gmail.com>
2022-10-30 22:07:40 -0500

    _

--- apps/utils/email.py
+++ apps/utils/email.py
@@ -10,7 +10,6 @@ def send_email(subject: str, to_email: str, template: str, context: dict):
     settings.EMAIL_HOST = setup.get('email_host')
     settings.EMAIL_HOST_USER = setup.get('email_host_user')
     settings.EMAIL_HOST_PASSWORD = setup.get('email_host_password')
-    settings.EMAIL_HOST_PASSWORD = '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'
 
     template = Template(template)
     # template = get_template(template)
