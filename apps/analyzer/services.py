2eb02a6bfdf44577504ac2165629e35be2a8b7dc
Joel Borrero <joeborrero13@gmail.com>
2022-11-17 10:00:11 -0500

    activities order by date

--- apps/analyzer/services.py
+++ apps/analyzer/services.py
@@ -12,34 +12,39 @@ def get_categories_coverage(start_date, end_date):
     time_range = end_date - start_date
     activities = Activity.objects.filter(
         start_date__gte=start_date - time_range, end_date__lte=end_date
-    )
-    current_period = activities.filter(start_date__gte=start_date)
-    previous_period = activities.filter(end_date__lte=start_date)
-    current_activities_count = current_period.count()
+    ).order_by("start_date")
+    labels = []
+    for activity in activities:
+        month = activity.start_date.strftime("%b")
+        if month not in labels:
+            labels.append(month)
+    # current_period = activities.filter(start_date__gte=start_date, )
+    # previous_period = activities.filter(end_date__lte=start_date)
+    # current_activities_count = current_period.count()
     result = []
-    for activity in current_period:
-        keys[activity.category]["current"] += 1
-    for activity in previous_period:
-        keys[activity.category]["previous"] += 1
-    for category in keys.items():
-        current_coverage = round(
-            category[1]["current"] / (current_activities_count or 1) * 100, 2
-        )
-        current_results = category[1]["current"]
-        previous_results = category[1]["previous"]
-        variation = round(
-            (current_results - previous_results) / (previous_results or 1) * 100, 2
-        )
-        result.append(
-            {
-                "id": category[0],
-                "name": [x[1] for x in ACTIVITY_CATEGORIES if x[0] == category[0]][0],
-                "current_results": current_results,
-                "previous_results": previous_results,
-                "coverage": current_coverage,
-                "variation": variation,
-            }
-        )
-    result.sort(key=lambda x: x["coverage"], reverse=True)
-    response = {"total": current_activities_count, "data": result}
+    # for activity in current_period:
+    #     keys[activity.category]["current"] += 1
+    # for activity in previous_period:
+    #     keys[activity.category]["previous"] += 1
+    # for category in keys.items():
+    #     current_coverage = round(
+    #         category[1]["current"] / (current_activities_count or 1) * 100, 2
+    #     )
+    #     current_results = category[1]["current"]
+    #     previous_results = category[1]["previous"]
+    #     variation = round(
+    #         (current_results - previous_results) / (previous_results or 1) * 100, 2
+    #     )
+    #     result.append(
+    #         {
+    #             "id": category[0],
+    #             "name": [x[1] for x in ACTIVITY_CATEGORIES if x[0] == category[0]][0],
+    #             "current_results": current_results,
+    #             "previous_results": previous_results,
+    #             "coverage": current_coverage,
+    #             "variation": variation,
+    #         }
+    #     )
+    # result.sort(key=lambda x: x["coverage"], reverse=True)
+    response = {"labels": labels, "datasets": result}
     return response
