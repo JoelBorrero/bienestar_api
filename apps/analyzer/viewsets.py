be37d5fe4c46636bac5da8fb02ec849929aba7c2
Carlos Venencia <carlos.venencia@hotmail.com>
2022-11-18 22:56:23 -0500

    Added endpoints for processing_main and processing_date

--- apps/analyzer/viewsets.py
+++ apps/analyzer/viewsets.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from .models import UploadedReport
 from .serializers import UploadedReportSerializer
-from .tasks import load_statistics, common_words_activities_months
+from .tasks import load_statistics, common_words_activities_months, processing_date, processing_main
 from apps.utils.serializers import EmptySerializer
 
 
@@ -30,6 +30,16 @@ class StatisticsViewSet(viewsets.GenericViewSet):
         start_date = data.get("start_date")
         end_date = data.get("end_date")
         data = common_words_activities_months(start_date, end_date)
+        return Response(data)
+
+    @action(detail=False, methods=["GET"])
+    def processing_date(self, _):
+        data = processing_date()
+        return Response(data)
+
+    @action(detail=False, methods=["GET"])
+    def processing_main(self, _):
+        data = processing_main()
         return Response(data)
 
 
